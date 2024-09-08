dfb935f34df02c7a874d20bbe471c2c94cc4a77d
Kika7536 <kika7536.p@gmail.com>
2024-09-08 15:36:21 +0300

    Добавьте файлы проекта.

---
+++ w.p.3.1/консоля.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int a, b;
+
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "-----Проверяем-----" << endl;
+
+	if (a < b) {
+		cout << "Наименьшее число: " << a << endl;
+	}
+	else if (a > b) {
+		cout << "Наименьшее число: " << b << endl;
+	}
+	else {
+		cout << "Числа равны!" << endl;
+	}
+}
